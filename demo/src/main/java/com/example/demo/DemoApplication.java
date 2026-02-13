6c3fc65f93e4c709af7886eda2c183fd0d93f604
theMungai <mungaithesimon@gmail.com>
2026-02-14 02:37:20 +0300

    Springboot tutorial

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,15 @@
-package com.themungai.SpringBootBasics;
+package com.example.demo;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringBootBasicsApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBasicsApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.themungai.SpringBootBasics;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootBasicsApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
